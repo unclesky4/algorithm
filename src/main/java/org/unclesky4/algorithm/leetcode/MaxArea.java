8addd6ea55ded7dee07cf1e04609eaf751d4fd08
unclesky4 <1292931210@qq.com>
2019-09-01 00:14:39 +0800

    letterCombinations, maxArea

---
+++ src/main/java/org/unclesky4/algorithm/leetcode/MaxArea.java
@@ -0,0 +1,51 @@
+package org.unclesky4.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxArea {
+	
+	/**
+	 * 双指针法
+	 * @param height
+	 * @return
+	 */
+	public static int maxArea(int[] height) {
+
+		int max = 0, length = height.length;
+		int tmp = 0;
+
+		int start = 0;
+		int end = length - 1;
+		while (end > start) {
+			tmp = (end - start) * Math.min(height[start], height[end]);
+			max = Math.max(tmp, max);
+			if (height[start] < height[end]) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+		System.out.println(maxArea(height));
+	}
+}
+
+/**
+ * 
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i,ai) 和 (i, 0)。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,8,6,2,5,4,8,3,7] 输出: 49
+ * 
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/container-with-most-water
+ * 
+ */
